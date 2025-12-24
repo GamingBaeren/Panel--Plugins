63b85ea69af1b7365005df5b245e9e361c414dd9
Boy132 <mail@boy132.de>
2025-12-24 23:30:16 +0100

    update snowflakes config defaults

--- snowflakes/config/snowflakes.php
+++ snowflakes/config/snowflakes.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'size' => env('SNOWFLAKES_SIZE', 1),
-    'speed' => env('SNOWFLAKES_SPEED', 3),
+    'size' => env('SNOWFLAKES_SIZE', 0.8),
+    'speed' => env('SNOWFLAKES_SPEED', 1),
     'opacity' => env('SNOWFLAKES_OPACITY', 0.5),
     'density' => env('SNOWFLAKES_DENSITY', 1),
     'quality' => env('SNOWFLAKES_QUALITY', 1),
